67c4d3dee9924eb0be3bb1c4e87ca7621b8d36e1
LDH <edh7054@gmail.com>
2023-07-11 12:09:38 +0900

    블로그 수정

--- lib/team_blog.dart
+++ lib/team_blog.dart
@@ -3,14 +3,52 @@ import 'package:provider/provider.dart';
 import 'member_detail.dart';
 import 'member_service.dart';
 import 'member_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class TeamBlog extends StatelessWidget {
+class TeamBlog extends StatefulWidget {
   const TeamBlog({super.key});
 
   @override
+  State<TeamBlog> createState() => _TeamBlogState();
+}
+
+// final List<String> entries = <String>['A', 'B', 'C'];
+
+class _TeamBlogState extends State<TeamBlog> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(child: Text("Team Blog"))
-    );
+    //
+    return Consumer<MemberService>(builder: (context, memberService, child) {
+      List<Member> memberList = MemberService().memberList; // memberList 불러오기
+
+      return Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: memberList.length,
+                itemBuilder: (BuildContext ctx, int index) {
+                  Member member = memberList[index];
+
+                  return ElevatedButton(
+                    onPressed: () async {
+                      final url = Uri.parse(member.blog);
+                      setState(() {});
+                      if (await canLaunchUrl(url)) {
+                        launchUrl(url);
+                      } else {
+                        // ignore: avoid_print
+                        print(member.blog);
+                      }
+                    },
+                    child: Text(member.name),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
