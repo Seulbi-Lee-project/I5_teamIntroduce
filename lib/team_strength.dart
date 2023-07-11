a78cda61a79e4e5685a4a0c9e7b26f2f07ec27ab
Seulbi-Lee-project <ifssfws@gmail.com>
2023-07-11 09:51:22 +0900

    team strength member Listview

--- lib/team_strength.dart
+++ lib/team_strength.dart
@@ -3,14 +3,33 @@ import 'package:provider/provider.dart';
 import 'member_detail.dart';
 import 'member_service.dart';
 import 'member_view.dart';
+import 'member_service.dart';
 
 class TeamStrength extends StatelessWidget {
   const TeamStrength({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Member> memberList = MemberService().memberList;
+
     return Scaffold(
-        body: Center(child: Text("Team Intro"))
+      appBar: AppBar(),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: ListView.builder(
+              itemCount: memberList.length,
+              itemBuilder: (context, index) {
+                Member member = memberList[index];
+                return ListTile(
+                  title: Text(member.name),
+                  subtitle: Text(
+                      member.merit.isEmpty ? "장점을 서술하지 않았습니다." : member.merit),
+                );
+              }),
+        ),
+      )),
     );
   }
 }
